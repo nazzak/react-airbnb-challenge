cdcfc748705ada4ffd55d56e9d5527fa30460641
Zakari Nazim SAIBI <saibi.nazim@gmail.com>
2020-09-16 20:25:18 +0200

    hotfix

--- src/Component/Banner/Banner.js
+++ src/Component/Banner/Banner.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Banner.css'
 import { Button } from "@material-ui/core"
 
+
+
 function Banner() {
+    const [showSearch, setShowSearch] = useState(false)
+
+    const toggleSearchButton = () => {
+        setShowSearch(!showSearch)
+    }
+
     return (
         <div className="banner">
             <div class="banner__search">
-                <Button className='banner__searchButton' variant='outlined'>
-                    Search Dates
+                <Button onClick={() => toggleSearchButton()} className='banner__searchButton' variant='outlined'>
+                {showSearch === true ? "Search Dates" : "Hide"} 
                 </Button>
+                {showSearch === true ?
+                        <h1>
+                            true
+                        </h1>
+                        :
+                        undefined
+                    }
             </div>
             <div class="banner__info">
                 <h1>
