f5c34b0815bfbe87af0d5c14dacbf2d8a02938a6
Zakari Nazim SAIBI <saibi.nazim@gmail.com>
2020-09-16 23:12:37 +0200

    wrap the app inside a router

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Header from './Component/Header/Header';
 import Home from './Component/home/Home';
 import Footer from './Component/Footer/Footer'
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
 function App() {
   return (
 
     
     <div className="app">
-
-        
+      <Router>
         <Header />
         <Home />
         <Footer />
+      </Router>
+        
     </div>
   );
 }
