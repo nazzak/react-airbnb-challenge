ca4e9ead47c381d926afeb98bc600984eb3bc7da
Zakari Nazim SAIBI <saibi.nazim@gmail.com>
2020-09-16 23:21:17 +0200

    added switch and  route

--- src/App.js
+++ src/App.js
@@ -5,19 +5,34 @@ import Home from './Component/home/Home';
 import Footer from './Component/Footer/Footer'
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from './Component/SearchPage/SearchPage';
 
 
 function App() {
   return (
 
-    
+
     <div className="app">
       <Router>
         <Header />
-        <Home />
+
+        <Switch>
+
+          {/* /search */}
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+
+          {/* / */}
+          <Route path="/">
+            <Home />
+          </Route>
+
+        </Switch>
+
         <Footer />
       </Router>
-        
+
     </div>
   );
 }
