af979fe7c6b2bb9a4bc7a066409b4d33546cc0ed
Zakari Nazim SAIBI <saibi.nazim@gmail.com>
2020-09-16 22:32:36 +0200

    added some styling

--- src/Component/Card/Card.js
+++ src/Component/Card/Card.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import "./Card.css"
 
-function Card() {
+function Card(props) { //props: src, title, price, description
     return (
         <div className="card">
-            
+            <img src={props.src} alt=""/>
+            {/* Create info */}
+            <h2>{props.title}</h2>
+            <h4>{props.description}</h4>
+            <h3>{props.price}</h3>
         </div>
     )
 }
