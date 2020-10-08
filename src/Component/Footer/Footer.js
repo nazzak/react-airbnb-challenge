ea5a343197dd2e80b598fdb34fedbcd0a989177c
Zakari Nazim SAIBI <saibi.nazim@gmail.com>
2020-10-08 23:16:18 +0200

    updated Footer

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
-    return (
-        <div className="footer">
-            <p>2020 Airbnb clone! No rights reserved - this is a demo application</p>
-            <p>Privacy . Terms . Sitemap . built by NAZZAK</p>
-        </div>
-    )
+  return (
+    <div className="footer">
+      <p>
+        2020 Airbnb clone built using ReactJs! No rights reserved - this demo
+        application is one of my web development projetcs
+      </p>
+      <p>built by NAZZAK</p>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
